8325c8b72d26bd9f22ebeae833e42d0e13829e91
Elisângela <eliscostarj@gmail.com>
2026-08-28 21:26:30 -0300

    Anotações aula 9

---
+++ AULA9/treinos-diversos.py
@@ -0,0 +1,37 @@
+# def contar_caractere(texto, caractere_produrado)
+
+# frutas = ("maçã", "banana", "laranja", "abacaxi", "uva","mamão", "abacate", "limão")
+
+# len(frutas)
+
+#TREINO FUNÇÕES
+
+# def dar_boas_vindas():
+#     print("-"*40)
+#     print("Bem vindo ao aplicativo!")
+#     print("-"*40)
+
+# print("início do programa")
+# dar_boas_vindas()
+# print("Meio do programa.")
+# dar_boas_vindas()
+# print("Fim do programa.")
+
+
+# def boas_vindas_personalizado(nome_da_pessoa):
+#     print("-"*40)
+#     print(f"Olá, {nome_da_pessoa}! Seja bem vindo!")
+#     print("-"*40)
+
+# boas_vindas_personalizado("ElisÂngela")
+
+def somar(a,b):
+    resultado = a+b
+    return resultado
+
+soma1 = somar(5, 10)
+soma2 = somar(100, 50)
+
+print(f"O primeiro resultado é :{soma1}")
+print(f"O segundo resultado é: {soma2}")
+
