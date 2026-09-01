4d8588c86922e91158be74d1f17312e579be449a
Elisângela <eliscostarj@gmail.com>
2026-08-31 21:25:44 -0300

    atualização do projeto final

---
+++ PROJETO-FINAL/projeto-final.py
@@ -0,0 +1,37 @@
+#1) cardápio: listar cardápio
+item = {"nome:" "preço:"}
+#2) cardápio: cadastrar prato
+cardapio = {item:"descrição:"}
+#3) cardápio: consultar prato
+#4) cardápio: atualizar prato
+#5) cardápio: alterar disponibilidade
+
+#6) mesas: listar mesas
+mesa =
+#7) mesas: consultar mesas
+#8) mesas: cadastrar mesas
+#9) mesas: alterar status mesa + vincular garçom à mesa
+#10
+#11) garçons: cadastrar garções - visão sistema
+atendente =
+#12) garçons: listar garções - visão sistema
+#13) garçons: consultar garções - visão sistema
+#14) garçons: alterar status garções - visão sistema
+#15) garçons: listar mesas do garçom - visão sistema
+
+#16) pedido: criar pedido - visão cliente
+pedido = {item: quant:}
+#17) pedido: adicionar item pedido - visão cliente
+#18) pedido: consultar item pedido e/ou remover item do pedido e/ou cancelar pedido - visão cliente
+#19) pedido: consultar pedido - visão cliente/ Lstar pedidos da mesa - visão garçom
+#20) pedido: calcular total do pedido - visão cliente
+
+#21) cozinha: consultar pedido - visão cliente/ Lstar pedidos da mesa - visão garçom
+#22) cozinha: enviar pedido para cozinha - visão restaurante
+#23) cozinha: atualizar preparo - visão cliente
+
+#24) pagamento: gerar número do pedido - visão restaurante
+#25) pagamento: buscar conta pelo número - visão restaurante
+#26) pagamento: registar pagamento - visão restaurante
+#27) pagamento: fechar conta - visão restaurante
+    # perguntar se deseja algo mais; liberar a mesa + liberar atendente/ mostrar a visualização do cliente e do restaurante 
